2706cabba21abdcdd66bbfb69899dedb75ea8565
Samir DERKAOUI <126152143+samderkaoui@users.noreply.github.com>
2025-12-30 22:54:31 +0100

    Remove tutorial link from master.sh

--- master.sh
+++ master.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 calico_version="3.31.3"
 
-# TUTO https://www.linuxtechi.com/install-kubernetes-on-rockylinux-almalinux/
 echo "[TASK 1] PREREQUIS"
 sudo firewall-cmd --permanent --add-port={6443,2379,2380,10250,10251,10252,10257,10259,179}/tcp
 sudo firewall-cmd --reload
