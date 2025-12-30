da40e4aa7fc23a8f98cd481e0db7c5528ce35b00
Samir DERKAOUI <126152143+samderkaoui@users.noreply.github.com>
2025-12-30 22:25:49 +0100

    Refactor requirements.sh for Kubernetes setup
    Updated Kubernetes installation script with version variable and modified repository settings.

--- requirements.sh
+++ requirements.sh
@@ -3,15 +3,14 @@
 # TUTO https://www.linuxtechi.com/install-kubernetes-on-rockylinux-almalinux/
 # Variables
 USER_NAME="vagrant"
+KUBE_VERSION="1.35.0"  # Ou la dernière stable : vérifier sur kubernetes.io
 
 echo "[TACHE 1] PREREQUIS (paquets , ssh sans clé, update)"
-
 sudo yum install firewalld wget curl vim -y
 sudo systemctl start firewalld 
 sudo systemctl enable firewalld
 sudo firewall-cmd --permanent --add-port=22/tcp
 sudo firewall-cmd --reload
-
 
 # Vérifiez si "#PasswordAuthentication no" ou "#PasswordAuthentication yes" est présent dans le fichier pour se connecter en ssh via mot de passe
 if grep -q "^#PasswordAuthentication no" /etc/ssh/sshd_config; then
@@ -29,9 +28,6 @@ sudo systemctl restart sshd
 sudo yum update -y
 
 
-
-
-
 echo "[TACHE 2] MAJ FICHIER HOST"
 echo '192.168.10.100 k8s-master' | sudo tee -a /etc/hosts
 echo '192.168.10.2 k8s-worker1' | sudo tee -a /etc/hosts
@@ -41,10 +37,8 @@ echo '192.168.10.3 k8s-worker2' | sudo tee -a /etc/hosts
 echo "[TACHE 3] CONFIGURER CONTAINER RUN TIME (CONTAINERD)"
 containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
 sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
-
 sudo systemctl restart containerd
 sudo systemctl enable containerd
-
 
 
 echo "[TACHE 4] DISABLE SWAP"
@@ -52,11 +46,9 @@ sudo swapoff -a
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
 
-
 echo "[TACHE 5] SELINUX PERMISSIVE"
 sudo setenforce 0
 sudo sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=permissive/g' /etc/sysconfig/selinux
-
 
 
 echo "[TACHE 6] AJOUTER DES MODULES ET DES PARAMÈTRES DU KERNEL"
@@ -74,30 +66,22 @@ net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward                 = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
-
 sysctl --system >/dev/null 2>&1
 
 
-
-
 echo "[TACHE 8] AJOUT K8S REPO"
-cat <<EOF | sudo tee /etc/yum.repos.d/k8srpm.repo
-[k8s]
-name=k8s
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.30/rpm/
 enabled=1
 gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-exclude=kubelet kubeadm kubectl
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.30/rpm/repodata/repomd.xml.key
 EOF
-# Les paquets kubelet kubeadm et kubectl
-#  sont exclus afin d’éviter tout update ultérieure non intentionnelle.
-
 
 
 echo "[TASK 9] INSTALLER KUBERNETES KUBEADM, KUBELET ET KUBECTL"
-sudo yum install -y kubelet kubeadm kubectl --disableexcludes=k8s
+dnf install -y kubelet-$KUBE_VERSION kubeadm-$KUBE_VERSION kubectl-$KUBE_VERSION --disableexcludes=kubernetes
 
 
 
